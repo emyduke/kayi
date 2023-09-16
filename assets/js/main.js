04deff0496bb506e3446d356b79a33c6c38a33e2
emyduke <emydukeifoulimre@gmail.com>
2023-09-16 16:08:35 +0100

    click to view carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,68 @@
 
-$('.carousel').slick({
-    dots: true,
-    infinite: true,
-    speed: 700,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
+// $('.sliderContainer').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 700,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     arrows: true,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
 
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 1
+//             }
+//         },
+
+//     ]
+// })
+
+
+
+$(document).ready(function () {
+    $(".sliderContainer").show();
+    $('.sliderContainer').slick({
+        arrows: false,
+        // dots: false,
+        infinite: true,
+        // speed: 300,
+        // slidesToShow: 3,
+        // adaptiveHeight: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        // cssEase: 'linear',
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 3,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '80px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '60px',
+                    slidesToShow: 1
+                }
             }
-        },
+        ]
+    });
 
-    ]
+
+    $(".sliderItem").on('click', function() {
+        bg = $(this).data("img");
+        $("body").css("background-image", `url(${bg})`)
+    })
 })
