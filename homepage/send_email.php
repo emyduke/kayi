5b348a0348ce58a88a1ff3835a55249ddfaa74d0
emyduke <emydukeifoulimre@gmail.com>
2023-09-23 12:28:33 +0100

    ...

--- homepage/send_email.php
+++ homepage/send_email.php
@@ -24,3 +24,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Handle cases where the form was not submitted
     echo "Form not submitted.";
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    please wait....
+</body>
+</html>
